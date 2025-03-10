49637fb1e00f493250de09a7e0ab3eb145072663
Felipe Robot <felipeuseche22@gmail.com>
2025-03-09 21:59:14 -0500

    Exercise 3.2, 3.3 &  3.4

--- part-3-Back-Agenda/index.js
+++ part-3-Back-Agenda/index.js
@@ -31,5 +31,65 @@ app.get('/api/persons', (req, res)=>{
 
 })
 
+app.get('/info', (req, res) => {
+    const numContacts = contacts.length;
+    const currentTime = new Date().toString();
+    
+    // Respuesta HTML
+    res.send(`
+      <div>
+        <p>Phonebook has info for ${numContacts} people.</p>
+        <p>${currentTime}</p>
+      </div>    
+    `);
+  });
+
+app.get('/:id',(req,res)=>{
+    
+    const id = Number(req.params.id);
+
+    try{
+        if(id>contacts.length){
+            res.status(404).json({
+                error: 'Register not found',
+                message: 'There is no register with that id'
+            })
+        }else{
+            res.send(contacts[id]);
+        }
+        
+    }catch(error){
+        res.send('There was an issue calling the endpoint: ', error);
+    }
+})
+
+app.delete('/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    try{
+        if(id>contacts.length){
+            res.status(400).json({
+                message: 'Register not found'
+            })
+        }else{
+            contacts.splice(id, 1);
+            res.status(200).json({
+                message: 'Register deleted succesfully.'
+            })
+        }
+    }catch(error){
+        console.error('There was an error deleting the register: ', error);
+        res.status(500).json({
+            message: 'There was an issue deleting the register. ${error}'
+        });
+    }
+
+    
+    
+
+
+});
+
+
+
 
 app.listen(8080);
